6c40870ae229f6eeb8dee0ad66d5737af771dcfd
Jadhiel Vélez <jadhielv@gmail.com>
2020-11-26 23:51:40 -0400

    Add unit tests for validators

---
+++ Backend/src/KCTest.Tests/Validators/PermissionTypeValidatorTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation.TestHelper;
+using KCTest.Domain.DTOs;
+using KCTest.Infrastructure.Validators;
+using NUnit.Framework;
+
+namespace KCTest.Tests.Validators
+{
+    [TestFixture]
+    public class PermissionTypeValidatorTests
+    {
+        private PermissionTypeValidator validator;
+
+        [SetUp]
+        public void Setup()
+        {
+            validator = new PermissionTypeValidator();
+        }
+
+        [Test]
+        public void Should_have_error_when_Description_is_null()
+        {
+            var model = new PermissionTypeDto { Description = null };
+            var result = validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(pt => pt.Description);
+        }
+
+        [Test]
+        public void Should_not_have_error_when_Description_is_specified()
+        {
+            var model = new PermissionTypeDto { Description = "Otros" };
+            var result = validator.TestValidate(model);
+            result.ShouldNotHaveValidationErrorFor(pt => pt.Description);
+        }
+    }
+}
