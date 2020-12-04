c1f44d7f20dc06621d1d60f582d1e3f50d12cb49
Jadhiel Vélez <jadhielv@gmail.com>
2020-12-04 00:02:59 -0400

    Fix implement 'IPermissionTypeService' interface

--- Backend/src/KCTest.Domain/Services/IPermissionTypeService.cs
+++ Backend/src/KCTest.Domain/Services/IPermissionTypeService.cs
@@ -6,7 +6,7 @@ namespace KCTest.Domain.Services
 {
     public interface IPermissionTypeService
     {
-        Task<Result<ResponseWithElement<PermissionDto>>> AddPermissionType(PermissionTypeDto permissionDto);
+        Task<Result<ResponseWithElement<PermissionTypeDto>>> AddPermissionType(PermissionTypeDto permissionDto);
         Task<Result<Response>> UpdatePermissionType(PermissionTypeDto permissionDto);
         Task<Result<Response>> DeletePermissionType(int permissionId);
         Task<Result<ResponseWithElement<PermissionTypeDto>>> GetPermissionType(int permissionId);
