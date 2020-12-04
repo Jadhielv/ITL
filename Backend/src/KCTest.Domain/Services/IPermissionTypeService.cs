1b6f3dd711213ad009eecea34e40648a36bf16f7
Jadhiel Vélez <jadhielv@gmail.com>
2020-12-04 00:23:42 -0400

    Refactoring: Backend

--- Backend/src/KCTest.Domain/Services/IPermissionTypeService.cs
+++ Backend/src/KCTest.Domain/Services/IPermissionTypeService.cs
@@ -6,10 +6,10 @@ namespace KCTest.Domain.Services
 {
     public interface IPermissionTypeService
     {
-        Task<Result<ResponseWithElement<PermissionTypeDto>>> AddPermissionType(PermissionTypeDto permissionDto);
-        Task<Result<Response>> UpdatePermissionType(PermissionTypeDto permissionDto);
-        Task<Result<Response>> DeletePermissionType(int permissionId);
-        Task<Result<ResponseWithElement<PermissionTypeDto>>> GetPermissionType(int permissionId);
+        Task<Result<ResponseWithElement<PermissionTypeDto>>> AddPermissionType(PermissionTypeDto permissionTypeDto);
+        Task<Result<Response>> UpdatePermissionType(PermissionTypeDto permissionTypeDto);
+        Task<Result<Response>> DeletePermissionType(int permissionTypeId);
+        Task<Result<ResponseWithElement<PermissionTypeDto>>> GetPermissionType(int permissionTypeId);
         Task<Result<ResponseWithList<PermissionTypeDto>>> GetPermissionTypes(Pagination pagination = null);
     }
 }
