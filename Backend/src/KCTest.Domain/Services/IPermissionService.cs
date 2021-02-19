3f8747006e5cd81101826f04cb24c9228c12baf9
nminaya <nelsonminayaperez@gmail.com>
2021-02-18 21:16:40 -0400

    Modify PermissionService

--- Backend/src/KCTest.Domain/Services/IPermissionService.cs
+++ Backend/src/KCTest.Domain/Services/IPermissionService.cs
@@ -1,15 +1,16 @@
 ﻿using KCTest.Domain.Common;
 using KCTest.Domain.DTOs;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace KCTest.Domain.Services
 {
     public interface IPermissionService
     {
-        Task<Result<ResponseWithElement<PermissionDto>>> AddPermission(PermissionDto permissionDto);
-        Task<Result<Response>> UpdatePermission(PermissionDto permissionDto);
-        Task<Result<Response>> DeletePermission(int permissionId);
-        Task<Result<ResponseWithElement<PermissionDto>>> GetPermission(int permissionId);
-        Task<Result<ResponseWithList<PermissionDto>>> GetPermissions(Pagination pagination = null);
+        Task<PermissionDto> AddPermission(PermissionDto permissionDto);
+        Task UpdatePermission(PermissionDto permissionDto);
+        Task DeletePermission(int permissionId);
+        Task<PermissionDto> GetPermission(int permissionId);
+        Task<IEnumerable<PermissionDto>> GetPermissions(Pagination pagination = null);
     }
 }
