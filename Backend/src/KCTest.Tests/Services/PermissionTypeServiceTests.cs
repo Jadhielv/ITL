64071875a2a85ffc57a6372a7765e00aecad68d7
Nelson Minaya <nelsonminayaperez@gmail.com>
2021-04-25 09:32:58 -0400

    #37: Create AddPermissionType_PermissionTypeDoesNotExist_Should_SavePermissionType test

--- Backend/src/KCTest.Tests/Services/PermissionTypeServiceTests.cs
+++ Backend/src/KCTest.Tests/Services/PermissionTypeServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using KCTest.Application.Services;
+﻿using AutoMapper;
+using KCTest.Application.Services;
 using KCTest.Domain;
 using KCTest.Domain.DTOs;
 using KCTest.Domain.Entities;
@@ -8,6 +9,7 @@ using Moq;
 using NUnit.Framework;
 using System;
 using System.Linq.Expressions;
+using System.Threading.Tasks;
 
 namespace KCTest.Tests.Services
 {
@@ -32,6 +34,32 @@ namespace KCTest.Tests.Services
 
             // Act => Assert
             Assert.ThrowsAsync<ConflictException>(() => service.AddPermissionType(permissionType), "The permission type already exist.");
+        }
+
+        [Test]
+        public async Task AddPermissionType_PermissionTypeDoesNotExist_Should_SavePermissionType()
+        {
+            // Arrange
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            var permissionTypeRepositoryMock = new Mock<IPermissionTypeRepository>();
+            var mapperMock = new Mock<IMapper>();
+
+            var service = new PermissionTypeService(unitOfWorkMock.Object, mapperMock.Object);
+
+            var permissionType = new PermissionTypeDto { };
+
+            permissionTypeRepositoryMock.Setup(x => x.ExistAsync(It.IsAny<Expression<Func<PermissionType, bool>>>()))
+                .ReturnsAsync(false);
+
+            unitOfWorkMock.Setup(x => x.PermissionTypeRepository)
+                .Returns(permissionTypeRepositoryMock.Object);
+
+            // Act
+            await service.AddPermissionType(permissionType);
+
+            // Assert
+            permissionTypeRepositoryMock.Verify(x => x.AddAsync(It.IsAny<PermissionType>()));
+            unitOfWorkMock.Verify(x => x.SaveAsync());
         }
     }
 }
