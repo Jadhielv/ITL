43cb6a841d3274e13fe92aa4dcbe1c078ec4a21c
Nelson Minaya <nelsonminayaperez@gmail.com>
2021-04-25 09:40:12 -0400

    #37: Create DeletePermissionType_PermissionTypeNotFound_Should_ThrowsNotFoundException test

--- Backend/src/KCTest.Tests/Services/PermissionTypeServiceTests.cs
+++ Backend/src/KCTest.Tests/Services/PermissionTypeServiceTests.cs
@@ -108,5 +108,24 @@ namespace KCTest.Tests.Services
             permissionTypeRepositoryMock.Verify(x => x.UpdateAsync(It.IsAny<PermissionType>()));
             unitOfWorkMock.Verify(x => x.SaveAsync());
         }
+
+        [Test]
+        public void DeletePermissionType_PermissionTypeNotFound_Should_ThrowsNotFoundException()
+        {
+            // Arrange
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            var permissionTypeRepositoryMock = new Mock<IPermissionTypeRepository>();
+
+            var service = new PermissionTypeService(unitOfWorkMock.Object, null);
+
+            permissionTypeRepositoryMock.Setup(v => v.ExistAsync(It.IsAny<Expression<Func<PermissionType, bool>>>()))
+                .ReturnsAsync(false);
+
+            unitOfWorkMock.Setup(v => v.PermissionTypeRepository)
+                .Returns(permissionTypeRepositoryMock.Object);
+
+            // Act => Assert
+            Assert.ThrowsAsync<NotFoundException>(() => service.DeletePermissionType(1), "The permission type doesn't exist.");
+        }
     }
 }
