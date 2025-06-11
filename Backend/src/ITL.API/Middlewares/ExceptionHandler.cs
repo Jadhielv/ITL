827a65952794da35b2c24be93228ef27cb7ce807
Jadhiel Vélez <jadhielv@gmail.com>
2025-06-11 15:18:59 -0400

    cleanup exception handler

--- Backend/src/ITL.API/Middlewares/ExceptionHandler.cs
+++ Backend/src/ITL.API/Middlewares/ExceptionHandler.cs
@@ -3,7 +3,6 @@ using ITL.Domain.Exceptions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using System;
-using System.IO;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
